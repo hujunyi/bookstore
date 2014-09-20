1a8a0681dbec364c51ce86f09ae53213ce2778f9
hujunyi <hujunyi@gmail.com>
2014-09-19 20:14:38 -0400

    Set up delayed job for production

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,8 +1,8 @@
 APP_PATH = "/var/www/depot"
 working_directory APP_PATH
 pid APP_PATH+"/tmp/pids/unicorn.pid"
-stderr_path APP_PATH+"/log/stderr.log"
-stdout_path APP_PATH+"/log/stdout.log"
+stderr_path APP_PATH+"/log/unicorn_stderr.log"
+stdout_path APP_PATH+"/log/unicorn_stdout.log"
 
 listen "/tmp/unicorn.depot.sock"
 worker_processes 2
